424a8e97cd913fa8a5bd60b974a33658c3b9f563
Ashraful610 <ashalam610@gmail.com>
2023-09-03 23:15:01 +0600

    finish two polytechnic

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 function submit(code , id){
-    const exitCode = code;
     let newCode = document.getElementById(id).value
-    if(newCode == exitCode) {
-        // alert(`Your code was correct for ${id}`);
-        window.location.href='home.html';
+    if(newCode == code) {
+        if(code == 12345){
+            window.location.href = 'bbpi.html'
+        }
+        else if ( code == 67890){
+            window.location.href = 'mbpi.html'
+        }
+        else if (code == 'bbpimale'){
+            window.location.href = 'bbpiMale.html'
+        }
+        else if ( code == 'bbpifemale'){
+            window.location.href = 'bbpiFemale.html'
+        }
+        else if ( code == 'mbpimale'){
+            window.location.href = 'mbpiMale.html'
+        }
+        else if ( code == 'mbpifemale'){
+            window.location.href = 'mbpiFemale.html'
+        }
         document.getElementById(id).value = ''
     }
     else{
